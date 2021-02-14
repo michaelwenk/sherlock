72f19d562d25853a18d929fc4590a89583ae8b92
michaelwenk <michael.wenk@uni-jena.de>
2021-02-14 13:33:01 +0100

    added nmr-displayer to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import QueryPanel from './component/panels/QueryPanel';
+import NMRDisplayer from 'nmr-displayer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to WebCASE !!!</p>
+        <NMRDisplayer />
+        <p>-----</p>
         <QueryPanel />
       </header>
     </div>
